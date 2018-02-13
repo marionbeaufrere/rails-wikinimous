0472f532f135b72bed91ec1fac26dd0671e891f4
Marion Beaufrere <marion.beaufrere@gmail.com>
2018-02-13 16:05:51 +0100

    seeded articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+
+def fake_content
+  paragraphs = []
+  paragraphs << Faker::Lorem.paragraph
+  paragraphs << Faker::Lorem.paragraph(2)
+  paragraphs << Faker::Lorem.paragraph(3)
+  paragraphs.flatten.join("\n\n")
+end
+
+10.times do
+  Article.create(
+    title: Faker::Company.catch_phrase,
+    content: fake_content
+  )
+end
